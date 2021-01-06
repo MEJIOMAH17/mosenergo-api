08852d033ba07a5d838bd2a95b46b9478117571d
Mark <mekosichkin@gmail.com>
2021-01-06 09:43:16 -0800

    add publish code

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,3 +16,23 @@ dependencies {
     implementation("io.github.rybalkinsd:kohttp:0+")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1+")
 }
+
+val githubToken: String by project
+val githubUser: String by project
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/MEJIOMAH17/mosenergo-api")
+            credentials {
+                username = githubUser
+                password = githubToken
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
